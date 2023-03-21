522792887e44ba71a0151d97818fe7058bf217c3
RhuanGv <rhuangabrielv@gmail.com>
2023-03-21 07:33:16 -0300

    Initial commit

---
+++ Atividade-app/app/Http/Controllers/Controller1.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller1 extends Controller
+{
+    public function index()
+    {
+        return 'Segunda rota!';
+        
+    }
+}
+
---
+++ Atividade-app/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+
+<a href="{{ route('hello') }}">Esta é a primeira rota, clique para ir a segunda</a>
+
+</div>
+
+</html>
